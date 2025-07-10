fbf3977ca01413c9dc274e1b36f23094eb16dc48
zhr520v <63360959+zhr520v@users.noreply.github.com>
2025-07-10 14:55:19 +0800

    Update util.go
    Signed-off-by: zhr520v <63360959+zhr520v@users.noreply.github.com>

--- drivers/123/util.go
+++ drivers/123/util.go
@@ -203,9 +203,10 @@ do:
 		"origin":        "https://www.123pan.com",
 		"referer":       "https://www.123pan.com/",
 		"authorization": "Bearer " + d.AccessToken,
-		"user-agent":    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) openlist-client",
-		"platform":      "web",
-		"app-version":   "3",
+		"user-agent":    "123pan/2.5.5(Android_13.1.2;Vivo)",
+		"platform":      "android",
+		"app-version":   "78",
+		"x-app-version": "2.5.5",
 		//"user-agent":    base.UserAgent,
 	})
 	if callback != nil {
