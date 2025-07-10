24468061732ee3cfa57de47312662ccbff33ae46
zhr520v <63360959+zhr520v@users.noreply.github.com>
2025-07-10 14:20:10 +0800

    Update meta.go
    Signed-off-by: zhr520v <63360959+zhr520v@users.noreply.github.com>

--- drivers/123/meta.go
+++ drivers/123/meta.go
@@ -8,8 +8,9 @@ import (
 type Addition struct {
 	Username string `json:"username" required:"true"`
 	Password string `json:"password" required:"true"`
+	FakeIP   string `json:"fake_ip" default:"120.229.187.66" help:"IP address to use for spoofing"`
 	driver.RootID
-	//OrderBy        string `json:"order_by" type:"select" options:"file_id,file_name,size,update_at" default:"file_name"`
+	//OrderBy string `json:"order_by" type:"select" options:"file_id,file_name,size,update_at" default:"file_name"`
 	//OrderDirection string `json:"order_direction" type:"select" options:"asc,desc" default:"asc"`
 	AccessToken string
 }
